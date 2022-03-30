31bf744350b6c6e8392e3ae7bb02190c941e8d8f
Abi-1-2 <101305374+Abi-1-2@users.noreply.github.com>
2022-03-30 16:58:59 +0530

    Add files via upload

---
+++ M1-Project-march-main/3_implementation/Multifile/markmain.c
@@ -0,0 +1,15 @@
+#include"mark.h"
+int main(){
+	input();
+	calculations();
+	 abi1();
+     abi2();
+     abi3();
+     abi4();
+     abi5();
+	resultfunction();
+	remarkfunction();
+	show();
+	return 0;
+	
+}
