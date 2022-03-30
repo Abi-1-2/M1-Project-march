07bce43b56dc979c20ff5816b7d9889efebcd136
Abi-1-2 <101305374+Abi-1-2@users.noreply.github.com>
2022-03-30 16:21:25 +0530

    Update marksheetmain.h

--- M1-Project-march-main/3_implementation/inc/marksheetmain.h
+++ M1-Project-march-main/3_implementation/inc/marksheetmain.h
@@ -1,36 +1,10 @@
-/**
- * @file marksheetmain.h
- * @author abi
- * @brief 
- * @version 0.1
- * @date 2022-03-28
- * 
- * @copyright Copyright (c) 2022
- * 
- */
+
 #ifndef __MARKSHEETMAIN__H_
 #define __MARKSHEETMAIN__H_
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-/**
- * @brief depends on user enter
- * @param[in] NAME
- * @param[in] PLACE
- * @param[in] COLLEGENAME
- * @param[in] MEDIUM
- * @param[in] ROLLNO
- * @param[in] SCIENCE
- * @param[in] SCIENCE1
- * @param[in] ELECTRIC CIRCUIT THEORY
- * @param[in] ELECTRIC CIRCUIT THEORY PRACTICALS
- * @param[in] EMBEDDED SYSTEM DESIGN
- * @param[in] EMBEDDED SYSTEM DESIGN PRACTICAL
- * @param[in] java programming
- * @param[in] java programming practical
- * @return show 
- * 
- */
+
 void input();
 void calculations();
 void abi1();
